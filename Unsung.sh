d8a017eecb09fad0942aea2652a9512a9ba95122
mkronvold <>
2023-07-22 20:42:05 -0500

    Unsung

---
+++ Unsung.sh
@@ -0,0 +1,8 @@
+./modparse.sh https://steamcommunity.com/sharedfiles/filedetails/?id=3006511687 Nak_Unsung_Req
+./modparse.sh https://steamcommunity.com/sharedfiles/filedetails/?id=3006516384 Nak_Unsung_Opt
+git st
+git add -A
+git commit -m "Unsung"
+git push
+cp PRESETS/Nak_Unsung_Req.html /mnt/p/FilePile/ArmA3/
+cp PRESETS/Nak_Unsung_Opt.html /mnt/p/FilePile/ArmA3/
