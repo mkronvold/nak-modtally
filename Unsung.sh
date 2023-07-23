6dc503907d76773e5726e99d5e70feb651d3247a
mkronvold <>
2023-07-22 20:44:22 -0500

    Unsung

--- Unsung.sh
+++ Unsung.sh
@@ -6,3 +6,10 @@ git commit -m "Unsung"
 git push
 cp PRESETS/Nak_Unsung_Req.html /mnt/p/FilePile/ArmA3/
 cp PRESETS/Nak_Unsung_Opt.html /mnt/p/FilePile/ArmA3/
+
+pushd /mnt/p/FilePile/ArmA3
+git st
+git add -A
+git commit -m "Unsung"
+git push
+popd
