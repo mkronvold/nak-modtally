c719cd4cb579c86e0cc138e14bab8f3954f20009
mkronvold <>
2023-12-09 11:57:24 -0600

    parse : and \

--- modparse.sh
+++ modparse.sh
@@ -66,9 +66,9 @@ esac
 FETCH=1
 
 [[ ${PARSE_TYPE} == "PRESET" ]] && xidel $1 -e '//tr / string-join(td, ",")' > ${IN}
-[[ ${PARSE_TYPE} == "PRESET" ]] && xidel --silent $1 -e '//tr / string-join(td, ",")' | awk -F, '{print "\"@" $1 "\";"}' | xargs | sed -e 's/@@/?@/g' -e 's/@/\"@/g' -e 's/;\ /;/g' -e 's/;/\";/g' -e 's/|/-/g' -e 's/\?\"@/\"@@/g' -e 's/\"@S.O.G. Prairie Fire\";//g'> ./setmods.txt
+[[ ${PARSE_TYPE} == "PRESET" ]] && xidel --silent $1 -e '//tr / string-join(td, ",")' | awk -F, '{print "\"@" $1 "\";"}' | xargs | sed -e 's/@@/?@/g' -e 's/@/\"@/g' -e 's/;\ /;/g' -e 's/;/\";/g' -e 's/|/-/g' -e 's/\//-/g' -e 's/:/-/g' -e 's/\?\"@/\"@@/g' -e 's/\"@S.O.G. Prairie Fire\";//g'> ./setmods.txt
 [[ ${PARSE_TYPE} == "COLLECTION" ]] && curl --silent $1 | grep href | grep class=\"workshopItemTitle\" | sed "s/>/\"/g" | sed "s/</\"/g" | awk -F\" '{print $9 ",Steam," $3}' > ${IN} && echo "**** Processing: ${1} ****"
-[[ ${PARSE_TYPE} == "COLLECTION" ]] && curl --silent $1 | grep href | grep class=\"workshopItemTitle\" | sed "s/>/\"/g" | sed "s/</\"/g" | awk -F\" '{print "\"@" $9 "\";"}' | xargs | sed -e 's/;/\";/g' -e 's/|/-/g' -e 's/\?\"@/\"@@/g' -e 's/\"@S.O.G. Prairie Fire\";//g'> ./setmods.txt && echo "**** Post-Processing: ${1} ****"
+[[ ${PARSE_TYPE} == "COLLECTION" ]] && curl --silent $1 | grep href | grep class=\"workshopItemTitle\" | sed "s/>/\"/g" | sed "s/</\"/g" | awk -F\" '{print "\"@" $9 "\";"}' | xargs | sed -e 's/;/\";/g' -e 's/|/-/g' -e 's/\//-/g' -e 's/:/-/g' -e 's/\?\"@/\"@@/g' -e 's/\"@S.O.G. Prairie Fire\";//g'> ./setmods.txt && echo "**** Post-Processing: ${1} ****"
 
 
 total=0
